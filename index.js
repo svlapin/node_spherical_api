7352b7775b83fd888c0e9f800082fda0bb867d2f
Sergey Lapin <sv0lapin@gmail.com>
2014-10-17 17:50:25 +0400

    additional barebones

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var server = require('./lib/server');
+var utils = require('./lib/utils');
+
+server.listen(process.env.PORT || 3000, function() {
+  console.log('%s server is running on port %d', utils.getTime(),
+    process.env.PORT || 3000);
+});
