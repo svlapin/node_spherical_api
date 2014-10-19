d65578558b7daa635e730ff2fd61bf9d8c4cc93f
Sergey <sv0lapin@gmail.com>
2014-10-19 20:46:56 +0400

    added displaying of the results

--- public/js/main.js
+++ public/js/main.js
@@ -7,11 +7,34 @@
         $scope.calculate = function() {
           $http.post('/calculate', $scope.formData)
             .success(function(data, status, headers, config) {
-              console.log(data)
+              $scope.error = null;
+              $scope.response = data;
             })
             .error(function(data, status, headers, config) {
-              console.log(data)
+              $scope.error = data;
+              $scope.response = null;
             });
+        };
+
+        $scope.formatMinutes = function(mins) {
+          var hrs = Math.floor(mins / 60);
+          var str = '';
+
+          if (hrs > 0) {
+            str += hrs.toString() + ' hours';
+            mins -= hrs * 60;
+          }
+
+          if (Math.floor(mins) > 0) {
+            str += ' ' + Math.floor(mins).toString() + ' minutes';
+          }
+
+          var secs = Math.round((mins - Math.floor(mins)) * 60);
+          if (secs > 0) {
+            str += ' ' + secs.toString() + ' seconds';
+          }
+
+          return str.trim();
         };
     }
   ]);
