9a95eea40981cf80ae00b7fc0d349ebd403590b2
Sergey <sv0lapin@gmail.com>
2014-10-19 15:29:20 +0400

    input UI

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
+(function() {
+  'use strict';
 
+  var app = angular.module('spherical-test', []);
+
+  app.controller('SphericalController', ['$scope', '$http',
+    function($scope, $http) {
+
+
+
+    }
+  ]);
+})();
